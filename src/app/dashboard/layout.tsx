b1df5ead657dd3c5414ca0baeac31db51eb2607c
k-azuki <teraoctit@gmail.com>
2025-09-28 23:31:36 +0900

    Create AppHeader / Create Page.tsx

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,12 +1,16 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/shared/AppSidebar"
+import { AppBreadcrumb } from "@/components/shared/AppHeader"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main>
-        <SidebarTrigger className="w-10 h-10" />
+      <main className="flex flex-col grow-1 w-full">
+        <div className="flex items-center space-x-4">
+            <SidebarTrigger className="w-12 h-12" />
+            <AppBreadcrumb />
+        </div>
         {children}
       </main>
     </SidebarProvider>
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,17 @@
+import { Waypoints } from "lucide-react"
+
 export default function Page() {
   return (
-    <div>page</div>
+    <div className="flex flex-col items-center justify-center grow-1 space-y-10">
+      <Waypoints className="w-64 h-64 text-blue-500" />
+      <div>
+        <h1 className="text-7xl font-bold">Welcome to Task Tree</h1>
+      </div>
+      <div className="flex flex-col items-center justify-center space-y-4">
+        <p className="text-4xl font-bold text-neutral-500">Select a workspace</p>
+        <p className="text-4xl font-bold text-neutral-500">or</p>
+        <p className="text-4xl font-bold text-neutral-500">Create a new workspace</p>
+      </div>
+    </div>
   )
 }
