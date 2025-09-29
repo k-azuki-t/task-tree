4dc8fec508745357a56bd041c9644c711e81cf6a
k-azuki <teraoctit@gmail.com>
2025-09-29 22:21:07 +0900

    create Dialog

--- src/components/shared/AppSidebar/WorkspaceList.tsx
+++ src/components/shared/AppSidebar/WorkspaceList.tsx
@@ -1,5 +1,6 @@
+'use client'
+
 import { Calendar, Home, Inbox, Search, Settings, Plus, MoreHorizontal } from "lucide-react"
- 
 import {
   SidebarGroup,
   SidebarGroupContent,
@@ -10,7 +11,26 @@ import {
   SidebarGroupAction,
   SidebarMenuAction,
 } from "@/components/ui/sidebar"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { 
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
+
  
 // Menu items.
 const items = [
@@ -46,7 +66,37 @@ export default function WorkspaceList() {
     <SidebarGroup>
         <SidebarGroupLabel>Workspace</SidebarGroupLabel>
         <SidebarGroupAction title="Add Project">
-            <Plus />
+            <Dialog>
+                <form>
+                    <DialogTrigger asChild>
+                    <Plus className="h-4" />
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>Open New Workspace</DialogTitle>
+                        <DialogDescription>
+                            Enter the new workspace name and descriptinon
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4">
+                        <div className="grid gap-3">
+                        <Label htmlFor="name">Description</Label>
+                        <Input id="name" name="name" />
+                        </div>
+                        <div className="grid gap-3">
+                        <Label htmlFor="description">Description</Label>
+                        <Input id="description" name="description" />
+                        </div>
+                    </div>
+                    <DialogFooter>
+                        <DialogClose asChild>
+                        <Button variant="outline">Cancel</Button>
+                        </DialogClose>
+                        <Button type="submit">Save changes</Button>
+                    </DialogFooter>
+                    </DialogContent>
+                </form>
+                </Dialog>
         </SidebarGroupAction>
         <SidebarGroupContent>
         <SidebarMenu>
